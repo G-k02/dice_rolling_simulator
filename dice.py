488ad5c7da77da91860782bfeb15926be6f31f09
Midhun Mathew <67965127+memidhun@users.noreply.github.com>
2021-10-19 23:48:38 +0530

    Changed some values
    Check that out!

--- dice.py
+++ dice.py
@@ -2,12 +2,12 @@ import random
 from tkinter import *
 
 root=Tk()
-root.title("Rolling dice")
+root.title("Rolling dice Programme")
 root.geometry("500x700")
 label=Label(root,text='',font=("times",260))
 
 def roll():
-    dice=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
+    dice=['\u2684','\u2685','\u2686','\u2687','\u2688','\u2689']
     label.configure(text=f'{random.choice(dice)}{random.choice(dice)}')
     label.pack()
 
